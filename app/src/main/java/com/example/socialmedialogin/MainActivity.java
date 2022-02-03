4da87ed8efcfaf10100f56e59936f6daf7a5845c
akashramkar <akashrramkar2001@gmail.com>
2022-02-03 18:47:11 +0530

    added dpi folders

--- app/src/main/java/com/example/socialmedialogin/MainActivity.java
+++ app/src/main/java/com/example/socialmedialogin/MainActivity.java
@@ -1,14 +1,85 @@
 package com.example.socialmedialogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
+public class MainActivity extends AppCompatActivity {
+private GoogleSignInOptions googleSignInOptions;
+private GoogleSignInAccount mGoogleSignInAccount;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        String userToken= String.valueOf(currentUser.getIdToken(false));
+//        updateUI(currentUser);
+    }
+    /*
+    private void createGoogleSignInRequest(){
+        googleSignInOptions=new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+//        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+    }
+
+     */
+
+        private void firebaseAuthWithGoogle(String idToken) {
+            AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+//                                Log.d(TAG, "signInWithCredential:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+//                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+//                                Log.w(TAG, "signInWithCredential:failure", task.getException());
+//                                Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+//                                updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+
+    void signOutuserWithGoogle(){
+        FirebaseAuth.getInstance().signOut();
+    }
+    void googleSignInUser(){
+//        Intent signInIntent=
     }
 }
